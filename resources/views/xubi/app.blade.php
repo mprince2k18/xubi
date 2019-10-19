e85bb1b8acfbbcb94471847e1237c990aa411b20
mprince2k18 <mprince2k16@gmail.com>
2019-10-19 14:14:44 +0600

    Minify v6

--- resources/views/xubi/app.blade.php
+++ resources/views/xubi/app.blade.php
@@ -499,8 +499,8 @@ border-radius: 0 0 8px 8px;
 <!-- js file start -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script src="{{ asset('xubisoft/assets/js/plugins.js') }}"></script>
-<script src="{{ asset('xubisoft/assets/js/Popper.js') }}"></script>
-<script src="{{ asset('xubisoft/assets/js/bootstrap.min.js') }}"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 <script src="{{ asset('xubisoft/assets/js/jquery.magnific-popup.min.js') }}"></script>
 <script src="{{ asset('xubisoft/assets/js/owl.carousel.min.js') }}"></script>
 <script src="{{ asset('xubisoft/assets/js/scrollax.js') }}"></script>
